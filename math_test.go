fb3beeac39f3f230631bf26318eabce785fdf759
pedro <pedrohenriquebecker00@gmail.com>
2024-09-13 15:17:34 -0300

    fix test

--- math_test.go
+++ math_test.go
@@ -4,9 +4,9 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	total := soma(15, 15)
+	total := soma(16, 15)
 
-	if total != 30 {
+	if total != 31 {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
