809f5aeef2afcbada0e367381e9fd083105ce150
pedro <pedrohenriquebecker00@gmail.com>
2024-09-14 16:19:08 -0300

    mod

--- math.go
+++ math.go
@@ -4,8 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println(soma(112, 10))
+	fmt.Println(notReachable())
+
 }
 
 func soma(a int, b int) int {
 	return a + b
+}
+
+func notReachable() int {
+	if true {
+		return 1
+	}
+
+	return 0
 }
